9eeb3670728e57505a70390ecd987626b962d974
satya sindhu <movva001@gmail.com>
2021-12-02 18:43:26 -0500

    ISS spotter changes

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+//  const { fetchMyIP } = require('./iss');
+const { fetchMyIP , fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
 
-// The code below is temporary and can be commented out.
-
+const { nextISSTimesForMyLocation } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
+
+fetchCoordsByIP("99.235.134.18" , (error,data) => {
+  console.log(error, data);
+});
+
+
+// fetchISSFlyOverTimes({ latitude: 43.693, longitude: -79.8365 },(error,data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyovertimes:' , data);
+// });
+
--- iss.js
+++ iss.js
@@ -30,22 +30,42 @@ const fetchMyIP = function(callback) {
 };
 
 const fetchCoordsByIP = function(ip, callback) {
-const url = `https://api.freegeoip.app/json/${ip}?apikey=6ac08040-3de2-11ec-81b5-2339b660aaa4`;
+  const url = `https://api.freegeoip.app/json/${ip}?apikey=6ac08040-3de2-11ec-81b5-2339b660aaa4`;
+  request(url , (error, response, body) => {
+if (error) {
+callback(error,null);
+} else {
+ // if non-200 status, assume server error
+if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+}
+const {latitude , longitude} = JSON.parse(body);
+ //callback(null,{latitude : res.latitude, longitude : res.longitude});
+fetchISSFlyOverTimes({latitude , longitude}, callback);
+  }
+ });
+};
+const fetchISSFlyOverTimes = function(coords, callback) {
+    // ...
+    const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
   
-    request(url , (error, response, body) => {
+    request(url , (error, res, body) => {
       if (error) {
         callback(error,null);
       } else {
         // if non-200 status, assume server error
-        if (response.statusCode !== 200) {
-          const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+        if (res.statusCode !== 200) {
+          const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
           callback(Error(msg), null);
           return;
         }
-        const {latitude , longitude} = JSON.parse(body);
-        //callback(null,{latitude : res.latitude, longitude : res.longitude});
-        fetchISSFlyOverTimes({latitude , longitude}, callback);
+        const {response} = JSON.parse(body);
+        //console.log(body);
+        callback(null,response);
       }
     });
   };
+  
 module.exports = { fetchMyIP };
